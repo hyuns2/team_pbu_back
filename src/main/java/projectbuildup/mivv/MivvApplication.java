1d32ab80bac494eb4551347e8062ed4fbfa521c1
이한솔 <hansol8701@naver.com>
2023-02-21 03:47:27 +0900

    unversioned file push

---
+++ src/main/java/projectbuildup/mivv/MivvApplication.java
@@ -0,0 +1,13 @@
+package projectbuildup.mivv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MivvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MivvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectbuildup/mivv/MivvApplicationTests.java
@@ -0,0 +1,13 @@
+package projectbuildup.mivv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MivvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
