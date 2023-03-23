67a5ee8d37fc0f90387858b4dda4d57e9f5c8098
ZZKIM <rkgml424@naver.com>
2023-03-23 15:12:47 +0900

    feat ChallengeRequestDto CreationDto

--- src/main/java/projectbuildup/mivv/domain/challenge/dto/request/ChallengeRequestDto.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/dto/request/ChallengeRequestDto.java
@@ -1,32 +1,48 @@
 package projectbuildup.mivv.domain.challenge.dto.request;
 
+import lombok.*;
 import projectbuildup.mivv.domain.challenge.entity.Challenge;
 
 import java.util.List;
 
 public class ChallengeRequestDto {
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class CreationRequest{
         String mainTitle;
         String subTitle;
         List<String> Content;
+        String imageUrl;
 
 
-        public Challenge toEntity{
-            //return Challenge;
+        public Challenge toEntity() {
+            return Challenge.builder()
+                    .mainTitle(mainTitle)
+                    .subTitle(subTitle)
+                    .Content(Content)
+                    .imageUrl(imageUrl)
+                    .build();
         }
 
     }
     public static class UpdateRequest {
 
-
+        int id;
+        String mainTitle;
+        String subTitle;
+        List<String> content;
+        String imageUrl;
 
     }
-    public static class ReadRequest {
 
+    public static class ReadRequest {
+        int id;
 
     }
     public static class DeleteRequest{
-
+        int id;
 
     }
 }
--- src/main/java/projectbuildup/mivv/domain/challenge/entity/Challenge.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/entity/Challenge.java
@@ -1,26 +1,42 @@
 package projectbuildup.mivv.domain.challenge.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 import projectbuildup.mivv.domain.member.entity.Member;
 import projectbuildup.mivv.global.common.BaseTimeEntity;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class Challenge extends BaseTimeEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    @Setter
+    @NonNull
     private String mainTitle;
+    @Setter
+    @NonNull
     private String subTitle;
-
+    @NonNull
     private LocalDate startDate;
+    @NonNull
     private LocalDate endDate;
-
+    @NonNull
+    @Setter
     private List<String> Content;
-
+    @NonNull
     private int remittanceOnceLimit;
+    @NonNull
     private int remittanceAvailableCount;
-
+    @NonNull
     private String imageUrl;
 
 
