13fcf460eb293f556b6229c10fd4e974abf178e6
ZZKIM <rkgml424@naver.com>
2023-03-23 15:18:43 +0900

    feat ChallengResponseDto ReadResponse

--- src/main/java/projectbuildup/mivv/domain/challenge/dto/response/ChallengeResponseDto.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/dto/response/ChallengeResponseDto.java
@@ -1,4 +1,24 @@
 package projectbuildup.mivv.domain.challenge.dto.response;
 
+import projectbuildup.mivv.domain.challenge.entity.Challenge;
+
+import java.util.List;
+
 public class ChallengeResponseDto {
+
+    public static class ReadResponse {
+        String mainTitle;
+        String subTitle;
+
+        List<String> content;
+
+        String imageUrl;
+
+        public ReadResponse(Challenge challenge){
+            this.mainTitle = challenge.getMainTitle();
+            this.subTitle = challenge.getSubTitle();
+            this.content = challenge.getContent();
+            this.imageUrl = challenge.getImageUrl();
+        }
+    }
 }
