191485f4129845dfcd42d47e752970d6216b5d9a
ZZKIM <rkgml424@naver.com>
2023-03-23 15:24:24 +0900

    feat ReadSpecificResponse in ChallengeResponseDto

--- src/main/java/projectbuildup/mivv/domain/challenge/dto/response/ChallengeResponseDto.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/dto/response/ChallengeResponseDto.java
@@ -1,7 +1,9 @@
 package projectbuildup.mivv.domain.challenge.dto.response;
 
+import lombok.NonNull;
 import projectbuildup.mivv.domain.challenge.entity.Challenge;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ChallengeResponseDto {
@@ -19,6 +21,31 @@ public class ChallengeResponseDto {
             this.subTitle = challenge.getSubTitle();
             this.content = challenge.getContent();
             this.imageUrl = challenge.getImageUrl();
+        }
+    }
+    public static class ReadSpecificResponse {
+        String mainTitle;
+        String subTitle;
+
+        List<String> content;
+
+        String imageUrl;
+
+        LocalDate startDate;
+        LocalDate endDate;
+
+        int remittanceOnceLimit;
+        int remittanceAvailableCount;
+
+        public ReadSpecificResponse(Challenge challenge){
+            this.mainTitle = challenge.getMainTitle();
+            this.subTitle = challenge.getSubTitle();
+            this.content = challenge.getContent();
+            this.imageUrl = challenge.getImageUrl();
+            this.startDate = challenge.getStartDate();
+            this.endDate = challenge.getEndDate();
+            this.remittanceOnceLimit = challenge.getRemittanceOnceLimit();
+            this.remittanceAvailableCount = challenge.getRemittanceAvailableCount();
         }
     }
 }
