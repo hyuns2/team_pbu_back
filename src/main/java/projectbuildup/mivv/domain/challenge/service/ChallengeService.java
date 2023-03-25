229c6061bd7126df1ea9c92193bb56fd78751637
ZZKIM <rkgml424@naver.com>
2023-03-25 18:49:46 +0900

    feat ChallengeValidationService : 챌린지 유효성 검사를 위한 서비스

--- src/main/java/projectbuildup/mivv/domain/challenge/service/ChallengeService.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/service/ChallengeService.java
@@ -1,4 +1,28 @@
 package projectbuildup.mivv.domain.challenge.service;
 
+import projectbuildup.mivv.domain.challenge.dto.request.ChallengeRequestDto;
+import projectbuildup.mivv.domain.challenge.dto.response.ChallengeResponseDto;
+import projectbuildup.mivv.domain.challenge.repository.ChallengeRepository;
+
 public class ChallengeService {
+
+    private final ChallengeRepository challengeRepository;
+    /*
+    * C :
+    * R
+    * U
+    * D
+    *
+    *
+    *
+    * */
+    public boolean paramValidate(String challengeId){
+
+        return true;
+    }
+    public void createChallenge(ChallengeRequestDto challengeRequestDto){
+
+
+    }
+
 }
