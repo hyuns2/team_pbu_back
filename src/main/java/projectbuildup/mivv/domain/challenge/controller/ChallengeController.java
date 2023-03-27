56959651769936ed9eb18033cac8888f05bda03c
ZZKIM <rkgml424@naver.com>
2023-03-27 19:11:11 +0900

    feat : createChallenge in ChallengeController : 챌린지 생성을 위한 챌린지컨트롤러 생성

--- src/main/java/projectbuildup/mivv/domain/challenge/controller/ChallengeController.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/controller/ChallengeController.java
@@ -1,4 +1,29 @@
 package projectbuildup.mivv.domain.challenge.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import projectbuildup.mivv.domain.challenge.dto.request.ChallengeRequestDto;
+import projectbuildup.mivv.domain.challenge.service.ChallengeService;
+import projectbuildup.mivv.domain.challenge.service.ChallengeValidationService;
+
+@Slf4j
+@RequestMapping("/api/challenges")
+@RequiredArgsConstructor
+@RestController
 public class ChallengeController {
+
+    private final ChallengeService challengeService;
+    private final ChallengeValidationService challengeValidationService;
+    @PostMapping("/create")
+    public ResponseEntity<HttpStatus> createChallenge(ChallengeRequestDto.CreationRequest challengeRequestDto){
+        challengeService.createChallenge(challengeRequestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
