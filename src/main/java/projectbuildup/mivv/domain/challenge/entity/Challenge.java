6da8360ad18c03e5bef727557d196de4598ef2c7
이한솔 <hansol8701@naver.com>
2023-04-04 21:51:20 +0900

    refactor: 챌린지와 챌린지 설명 테이블을 @ElementCollection을 이용해 일대다 관계로 매핑

--- src/main/java/projectbuildup/mivv/domain/challenge/entity/Challenge.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/entity/Challenge.java
@@ -1,15 +1,12 @@
 package projectbuildup.mivv.domain.challenge.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import projectbuildup.mivv.domain.challenge.dto.request.ChallengeRequestDto;
-import projectbuildup.mivv.domain.member.entity.Member;
 import projectbuildup.mivv.global.common.BaseTimeEntity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 @Entity
 @NoArgsConstructor
@@ -20,21 +17,15 @@ public class Challenge extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NonNull
     private String mainTitle;
-    @NonNull
     private String subTitle;
-    @NonNull
     private LocalDate startDate;
-    @NonNull
     private LocalDate endDate;
-    @NonNull
-    private List<String> content;
-    @NonNull
+    @ElementCollection
+    private List<String> content = new ArrayList<>();
+
     private int remittanceOnceLimit;
-    @NonNull
     private int remittanceAvailableCount;
-    @NonNull
     private String imageUrl;
 
     public void updateChallenge(ChallengeRequestDto.UpdateRequest updateRequestDto){
