9610c1c8d38becaf3de09c56378766d1c8b82761
ZZKIM <rkgml424@naver.com>
2023-04-12 21:59:54 +0900

    feat : 사용자가 쿠폰 발급 받는 로직 완성

--- src/main/java/projectbuildup/mivv/domain/couponIssuance/controller/CouponIssuanceController.java
+++ src/main/java/projectbuildup/mivv/domain/couponIssuance/controller/CouponIssuanceController.java
@@ -1,5 +1,24 @@
 package projectbuildup.mivv.domain.couponIssuance.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import projectbuildup.mivv.domain.couponIssuance.service.CouponIssuanceService;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "[CouponIssuance]", description = "사용자의 쿠폰 발급 관련 API입니다.")
 public class CouponIssuanceController {
+    private final CouponIssuanceService couponIssuanceService;
+
+    public ResponseEntity<HttpStatus> issueCouponToUser(Long userId, Long couponId){
+        couponIssuanceService.getCoupon(userId, couponId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
--- src/main/java/projectbuildup/mivv/domain/couponIssuance/service/CouponIssuanceService.java
+++ src/main/java/projectbuildup/mivv/domain/couponIssuance/service/CouponIssuanceService.java
@@ -8,6 +8,7 @@ import projectbuildup.mivv.domain.couponIssuance.entity.CouponIssuance;
 import projectbuildup.mivv.domain.couponIssuance.repository.CouponIssuanceRepository;
 import projectbuildup.mivv.domain.user.entity.User;
 import projectbuildup.mivv.domain.user.repository.UserRepository;
+import projectbuildup.mivv.global.error.exception.CBadRequestException;
 import projectbuildup.mivv.global.error.exception.CCouponNotFoundException;
 import projectbuildup.mivv.global.error.exception.CUserNotFoundException;
 
@@ -18,11 +19,19 @@ public class CouponIssuanceService {
     private final UserRepository userRepository;
     private final CouponRepository couponRepository;
 
-    public void getCouponForUser(Long userId, Long couponId){
+    public void getCoupon(Long userId, Long couponId){
         User user = userRepository.findById(userId).orElseThrow(CUserNotFoundException::new);
         Coupon coupon = couponRepository.findById(couponId).orElseThrow(CCouponNotFoundException::new);
+        isIssuable(user, coupon);
+        issue(user, coupon);
+    }
+    public void isIssuable(User user, Coupon coupon){
+        if(couponIssuanceRepository.findByUserAndCoupon(user, coupon).isPresent()){
+            throw new CBadRequestException("이미 보유중인 쿠폰입니다.");
+        }
+    }
+    public void issue(User user, Coupon coupon){
         CouponIssuance couponIssuance = new CouponIssuance(user, coupon);
         couponIssuanceRepository.save(couponIssuance);
-
     }
 }
