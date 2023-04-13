4a680711b68b0738cf231a9bfb4443f59fefd2f1
ZZKIM <rkgml424@naver.com>
2023-04-13 15:25:37 +0900

    fix : 가치소비컨트롤러에 설명 주석 추가

--- src/main/java/projectbuildup/mivv/domain/worthyConsumption/controller/WorthyConsumptionController.java
+++ src/main/java/projectbuildup/mivv/domain/worthyConsumption/controller/WorthyConsumptionController.java
@@ -25,11 +25,26 @@ public class WorthyConsumptionController {
     private final WorthyConsumptionService worthyConsumptionService;
     private final WorthyConsumptionValidationService worthyConsumptionValidationService;
     private final CouponService couponService;
+
+    /**
+     * 가치소비를 생성합니다.
+     * @param worthyConsumptionRequestDto
+     * @return
+     */
     @PostMapping
     public ResponseEntity<HttpStatus> createWorthyConsumption(@Valid @RequestBody WorthyConsumptionRequestDto.CreationRequest worthyConsumptionRequestDto){
             worthyConsumptionService.createWorthyConsumption(worthyConsumptionRequestDto);
             return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    /**
+     * 가치소비를 조회합니다.
+     * summary는 내가 찜한 가치소비 페이지에 들어갈 정보입니다.
+     * basic은 소비추천화면입니다. (SCR_Value)에 해당하는 정보입니다.
+     * detail은 소비추천화면에서 더 알아보기를 눌렀을 경우 가치소비에 대한 상세 정보를 담았습니다.
+     * @param worthyConsumptionId
+     * @return
+     */
     @GetMapping("/{worthyConsumptionId}/summary")
     public ResponseEntity<WorthyConsumptionResponseDto.ReadSummaryResponse> getWorthyConsumptionSummary(@PathVariable(name = "worthyConsumptionId") Long worthyConsumptionId){
         WorthyConsumptionResponseDto.ReadSummaryResponse worthyConsumptionResponseDto = worthyConsumptionService.readSummaryWorthyConsumption(worthyConsumptionId);
@@ -45,6 +60,20 @@ public class WorthyConsumptionController {
         WorthyConsumptionResponseDto.ReadDetailResponse WorthyConsumptionResponseDto = worthyConsumptionService.readDetailWorthyConsumption(worthyConsumptionId);
         return new ResponseEntity<>(WorthyConsumptionResponseDto, HttpStatus.OK);
     }
+
+    /**
+     *
+     * 가치소비의 여러 정보 수정 로직들 입니다.
+     * content는 제목, 내용, 태그 등
+     * url은 가치소비의 이미지나 영상의 url
+     * price는 가치소비의 가격
+     * place는 가치소비의 위치
+     * date는 가치소비의 날짜에 해당합니다.
+     *
+     * @param worthyConsumptionId
+     * @param worthyConsumptionRequestDto
+     * @return
+     */
     @PutMapping("/{worthyConsumptionId}/content")
     public ResponseEntity<HttpStatus> updateWorthyConsumptionContent(@PathVariable(name = "worthyConsumptionId") Long worthyConsumptionId, @Valid @RequestBody WorthyConsumptionRequestDto.UpdateContentRequest worthyConsumptionRequestDto){
         worthyConsumptionValidationService.isSameWorthyConsumptionId(worthyConsumptionId, worthyConsumptionRequestDto.getId());
@@ -75,11 +104,26 @@ public class WorthyConsumptionController {
         worthyConsumptionService.updateIssuableCouponDate(worthyConsumptionRequestDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    /**
+     * 가치소비를 삭제합니다.
+     *
+     * @param worthyConsumptionId
+     * @return
+     */
     @DeleteMapping("/{worthyConsumptionId}")
     public ResponseEntity<HttpStatus> deleteWorthyConsumption(@PathVariable(name = "worthyConsumptionId") Long worthyConsumptionId){
         worthyConsumptionService.deleteWorthyConsumption(worthyConsumptionId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    /**
+     * 가치소비에서 쿠폰을 생성하는 로직입니다.
+     *
+     * @param worthyConsumptionId
+     * @param couponRequestDto
+     * @return
+     */
     @PostMapping("/{worthyConsumptionId}")
     public ResponseEntity<HttpStatus> createCoupon(@PathVariable(name = "worthyConsumptionId") Long worthyConsumptionId, @Valid @RequestBody CouponRequestDto.CreationRequest couponRequestDto){
         couponService.createCoupon(worthyConsumptionId, couponRequestDto);
