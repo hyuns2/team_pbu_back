9fbec659a65aac1d49fd7d82e247691df3673323
ZZKIM <rkgml424@naver.com>
2023-04-13 09:43:12 +0900

    fix : 쿠폰컨트롤러에 swagger ui annotation 추가

--- src/main/java/projectbuildup/mivv/domain/coupon/controller/CouponController.java
+++ src/main/java/projectbuildup/mivv/domain/coupon/controller/CouponController.java
@@ -1,11 +1,13 @@
 package projectbuildup.mivv.domain.coupon.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import projectbuildup.mivv.domain.coupon.dto.request.CouponRequestDto;
 import projectbuildup.mivv.domain.coupon.dto.response.CouponResponseDto;
@@ -17,21 +19,29 @@ import projectbuildup.mivv.domain.coupon.service.CouponService;
 @Tag(name = "[Coupon]", description = "쿠폰 관련 API입니다.")
 public class CouponController {
     private final CouponService couponService;
+    @Operation(summary = "쿠폰을 생성 합니다.", description = "")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{couponId}")
     public ResponseEntity<CouponResponseDto.ReadResponseWithWorthyConsumption> readCouponWithWorthyConsumption(@PathVariable(name = "couponId") Long couponId){
         CouponResponseDto.ReadResponseWithWorthyConsumption couponResponseDto = couponService.readCouponWithWorthyConsumption(couponId);
         return new ResponseEntity<>(couponResponseDto, HttpStatus.OK);
     }
+    @Operation(summary = "쿠폰의 내용을 수정 합니다.", description = "")
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{couponId}/content")
     public ResponseEntity<HttpStatus> updateCouponContent(@PathVariable(name = "couponId") Long couponId, @Valid @RequestBody CouponRequestDto.UpdateContentRequest couponRequestDto){
         couponService.updateCouponContent(couponRequestDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+    @Operation(summary = "쿠폰의 날짜를 수정 합니다.", description = "")
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{couponId}/date")
     public ResponseEntity<HttpStatus> updateCouponDate(@PathVariable(name = "couponId") Long couponId, @Valid @RequestBody CouponRequestDto.UpdateDateRequest couponRequestDto){
         couponService.updateCouponDate(couponRequestDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+    @Operation(summary = "쿠폰을 삭제 합니다.", description = "")
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{couponId}")
     public ResponseEntity<HttpStatus> deleteCoupon(@PathVariable(name = "couponId") Long couponId){
         couponService.deleteCoupon(couponId);
