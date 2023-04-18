6b35c9973b95ec57536a8114c2a479a260e681f3
이한솔 <hansol8701@naver.com>
2023-04-19 03:33:49 +0900

    feat(RedisRankingSystem): 본인 인근의 순위를 조회하는 기능 추가

--- src/main/java/projectbuildup/mivv/domain/challenge/service/RedisRankingSystem.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/service/RedisRankingSystem.java
@@ -6,16 +6,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
 public class RedisRankingSystem {
     private final StringRedisTemplate redisTemplate;
     private ZSetOperations<String, String> operations;
+    private final static int NEARBY_SIZE = 2;
+
 
     @PostConstruct
+
     void init() {
         operations = redisTemplate.opsForZSet();
     }
@@ -62,5 +64,40 @@ public class RedisRankingSystem {
         Double score = Objects.requireNonNull(tuple.getScore());
         operations.add(key, member, score);
         return Long.parseLong(member);
+    }
+
+    /**
+     * 본인의 랭킹 +- (NEARBY_SIZE)명의 순위를 조회합니다.
+     *
+     * @param challengeId key
+     * @param userId      member
+     * @return 랭킹 리스트
+     */
+    public List<Optional<Long>> getNearbyRanking(Long challengeId, Long userId) {
+        String key = String.valueOf(challengeId);
+        String member = String.valueOf(userId);
+
+        Set<String> tupleSet = Objects.requireNonNull(operations.reverseRange(key, 0, -1));
+        return rankingList(member, tupleSet);
+    }
+
+    private List<Optional<Long>> rankingList(String member, Set<String> tupleSet) {
+        List<String> tupleList = tupleSet.stream().toList();
+        int targetIdx = tupleList.indexOf(member);
+
+        List<Optional<Long>> result = new ArrayList<>();
+        for (int i = targetIdx - NEARBY_SIZE; i <= targetIdx + NEARBY_SIZE; i++) {
+            result.add(getOptionalLong(tupleList, i));
+        }
+        return result;
+    }
+
+    private Optional<Long> getOptionalLong(List<String> list, int idx) {
+        try {
+            return Optional.of(Long.parseLong(list.get(idx)));
+        } catch (IndexOutOfBoundsException e) {
+            return Optional.empty();
+        }
+
     }
 }
