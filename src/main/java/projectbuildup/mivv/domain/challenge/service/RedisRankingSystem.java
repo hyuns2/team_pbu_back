0da91311df32b63bcb9b3d9dcbdd473964f7c63c
이한솔 <hansol8701@naver.com>
2023-04-19 02:56:00 +0900

    feat(RedisRankingSystem): 절약 점수를 초기화하는 메서드 구현

---
+++ src/main/java/projectbuildup/mivv/domain/challenge/service/RedisRankingSystem.java
@@ -0,0 +1,36 @@
+package projectbuildup.mivv.domain.challenge.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RedisRankingSystem {
+    private final StringRedisTemplate redisTemplate;
+    private ZSetOperations<String, String> operations;
+
+    @PostConstruct
+    void init() {
+        operations = redisTemplate.opsForZSet();
+    }
+
+    /**
+     * 해당 챌린지 랭킹의 절약 점수를 0으로 초기화합니다.
+     *
+     * @param challengeId key
+     * @param userId      member
+     */
+    public void insertZeroScore(Long challengeId, Long userId) {
+        String key = String.valueOf(challengeId);
+        String member = String.valueOf(userId);
+        Double score = operations.score(key, member);
+        if (score == null) {
+            operations.add(key, member, 0);
+            return;
+        }
+        operations.incrementScore(key, member, -score);
+    }
+}
