af1974dde8e08f3059f8524283c34e9995bb4d56
이한솔 <hansol8701@naver.com>
2023-04-19 04:04:52 +0900

    refactor(RedisRankingSystem): 랭킹 조회 시, 순위를 함께 반환하도록 수정

---
+++ src/main/java/projectbuildup/mivv/domain/challenge/dto/RankDto.java
@@ -0,0 +1,11 @@
+package projectbuildup.mivv.domain.challenge.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RankDto {
+    long rank;
+    long userId;
+}
--- src/main/java/projectbuildup/mivv/domain/challenge/service/RedisRankingSystem.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/service/RedisRankingSystem.java
@@ -1,10 +1,12 @@
 package projectbuildup.mivv.domain.challenge.service;
 
 import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
+import projectbuildup.mivv.domain.challenge.dto.RankDto;
 
 import java.util.*;
 
@@ -17,7 +19,6 @@ public class RedisRankingSystem {
 
 
     @PostConstruct
-
     void init() {
         operations = redisTemplate.opsForZSet();
     }
@@ -25,12 +26,10 @@ public class RedisRankingSystem {
     /**
      * 해당 챌린지 랭킹의 절약 점수를 0으로 초기화합니다.
      *
-     * @param challengeId key
-     * @param userId      member
+     * @param key    key
+     * @param member member
      */
-    public void initZero(Long challengeId, Long userId) {
-        String key = String.valueOf(challengeId);
-        String member = String.valueOf(userId);
+    public void initZero(String key, String member) {
         Double score = operations.score(key, member);
         if (score == null) {
             operations.add(key, member, 0);
@@ -42,23 +41,21 @@ public class RedisRankingSystem {
     /**
      * 해당 챌린지 랭킹의 절약 점수를 갱신합니다.
      *
-     * @param challengeId key
-     * @param userId      member
+     * @param key    key
+     * @param member member
+     * @param score  점수
      */
-    public void incrementScore(Long challengeId, Long userId, double score) {
-        String key = String.valueOf(challengeId);
-        String member = String.valueOf(userId);
+    public void incrementScore(String key, String member, double score) {
         operations.incrementScore(key, member, score);
     }
 
     /**
      * 해당 챌린지의 1등을 반환합니다.
      *
-     * @param challengeId key
+     * @param key key
      * @return 1등
      */
-    public Long getTheFirst(Long challengeId) {
-        String key = String.valueOf(challengeId);
+    public Long getTheFirst(String key) {
         ZSetOperations.TypedTuple<String> tuple = Objects.requireNonNull(operations.popMax(key));
         String member = Objects.requireNonNull(tuple.getValue());
         Double score = Objects.requireNonNull(tuple.getScore());
@@ -69,35 +66,46 @@ public class RedisRankingSystem {
     /**
      * 본인의 랭킹 +- (NEARBY_SIZE)명의 순위를 조회합니다.
      *
-     * @param challengeId key
-     * @param userId      member
+     * @param key    key
+     * @param member member
      * @return 랭킹 리스트
      */
-    public List<Optional<Long>> getNearbyRanking(Long challengeId, Long userId) {
-        String key = String.valueOf(challengeId);
-        String member = String.valueOf(userId);
+    public List<RankDto> getNearbyRanking(String key, String member) {
+        List<String> tupleList = Objects.requireNonNull(operations.reverseRange(key, 0, -1)).stream()
+                .toList();
+        List<Long> userRanking = getUserRanking(member, tupleList);
 
-        Set<String> tupleSet = Objects.requireNonNull(operations.reverseRange(key, 0, -1));
-        return rankingList(member, tupleSet);
-    }
-
-    private List<Optional<Long>> rankingList(String member, Set<String> tupleSet) {
-        List<String> tupleList = tupleSet.stream().toList();
-        int targetIdx = tupleList.indexOf(member);
-
-        List<Optional<Long>> result = new ArrayList<>();
-        for (int i = targetIdx - NEARBY_SIZE; i <= targetIdx + NEARBY_SIZE; i++) {
-            result.add(getOptionalLong(tupleList, i));
+        List<RankDto> result = new ArrayList<>();
+        long rank = Objects.requireNonNull(operations.reverseRank(key, member)) - NEARBY_SIZE;
+        for (Long userId : userRanking) {
+            addRank(result, rank, userId);
+            rank++;
         }
         return result;
     }
 
-    private Optional<Long> getOptionalLong(List<String> list, int idx) {
-        try {
-            return Optional.of(Long.parseLong(list.get(idx)));
-        } catch (IndexOutOfBoundsException e) {
-            return Optional.empty();
-        }
+    private List<Long> getUserRanking(String member, List<String> tupleList) {
+        int targetIdx = tupleList.indexOf(member);
 
+        List<Long> result = new ArrayList<>();
+        for (int i = targetIdx - NEARBY_SIZE; i <= targetIdx + NEARBY_SIZE; i++) {
+            result.add(getLong(tupleList, i));
+        }
+        return result;
+    }
+
+    private Long getLong(List<String> list, int idx) {
+        try {
+            return Long.parseLong(list.get(idx));
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
+    private void addRank(List<RankDto> result, long rank, Long userId) {
+        if (userId != null) {
+            RankDto rankData = new RankDto(rank, userId);
+            result.add(rankData);
+        }
     }
 }
