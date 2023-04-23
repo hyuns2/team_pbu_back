81cdb02c2f6b609de6da4a0753018f82e84239e9
ZZKIM <rkgml424@naver.com>
2023-04-23 22:56:53 +0900

    fix : 쇼츠 CRUD 일부 수정, 컨트롤러 완성

--- src/main/java/projectbuildup/mivv/domain/shorts/controller/Shortscontroller.java
+++ src/main/java/projectbuildup/mivv/domain/shorts/controller/Shortscontroller.java
@@ -1,4 +1,66 @@
 package projectbuildup.mivv.domain.shorts.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import projectbuildup.mivv.domain.shorts.dto.ShortsDto;
+import projectbuildup.mivv.domain.shorts.repository.ShortsRepository;
+import projectbuildup.mivv.domain.shorts.service.ShortsService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/shorts")
 public class Shortscontroller {
+  private final ShortsService shortsService;
+  @Operation(summary = "쇼츠 생성", description = "쇼츠를 생성합니다.")
+  @PostMapping
+  public ResponseEntity<HttpStatus> createShorts(@Valid @RequestBody ShortsDto.creatRequest shortsRequestDto){
+      shortsService.createShorts(shortsRequestDto);
+      return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+  @Operation(summary = "쇼츠 단건 조회", description = "쇼츠 하나를 조회합니다.")
+  @GetMapping("/{shortsId}")
+  public ResponseEntity<ShortsDto.shortsResponse> getOneShorts(@PathVariable(name = "shortsId") Long shortsId){
+      ShortsDto.shortsResponse shortsResponseDto = shortsService.getShorts(shortsId);
+      return new ResponseEntity<>(shortsResponseDto, HttpStatus.OK);
+  }
+  @Operation(summary = "절약 쇼츠 조회", description = "절약 쇼츠를 모두 조회합니다.")
+  @GetMapping("/saving")
+  public ResponseEntity<List<ShortsDto.shortsResponse>> getAllSavingShorts(){
+      List<ShortsDto.shortsResponse> shortsResponseDtoList = shortsService.getAllSavingShorts();
+      return new ResponseEntity<>(shortsResponseDtoList, HttpStatus.OK);
+  }
+  @Operation(summary = "소비 교육 쇼츠 조회", description = "소비 교육 쇼츠를 모두 조회합니다.")
+  @GetMapping("/edu")
+  public ResponseEntity<List<ShortsDto.shortsResponse>> getAllEduShorts(){
+      List<ShortsDto.shortsResponse> shortsResponseDtoList = shortsService.getAllEduShorts();
+      return new ResponseEntity<>(shortsResponseDtoList, HttpStatus.OK);
+  }
+  @Operation(summary = "쇼츠 수정", description = "쇼츠를 수정합니다.")
+  @PutMapping("/{shortsId}")
+  public ResponseEntity<HttpStatus> updateShorts(@PathVariable(name = "shortsId") Long shortsId, @RequestBody ShortsDto.updateRequest shortsRequestDto){
+      shortsRequestDto.setId(shortsId);
+      shortsService.updateShorts(shortsRequestDto);
+      return new ResponseEntity<>(HttpStatus.OK);
+  }
+  @Operation(summary = "쇼츠 삭제", description = "쇼츠를 삭제합니다.")
+  @DeleteMapping ("/{shortsId}")
+  public ResponseEntity<HttpStatus> deleteShorts(@PathVariable(name = "shortsId") Long shortsId){
+      ShortsDto.idRequest shortsRequestDto;
+      //shortsService.deleteShorts(new ShortsDto.idRequest(shortsId));
+      shortsService.deleteShorts(shortsId);
+      /*
+      고민) Long shortsId는 객체.. 니까 이것도 이 자체를 넘겨주는게 맞는 건가,, DTO로 감싸서 넘겨주는게 맞는거 같기도 한데.. 솔은 이렇게 안짰는데 이게 맞는 걸까유..ㅠ
+       */
+      return new ResponseEntity<>(HttpStatus.OK);
+  }
+
 }
--- src/main/java/projectbuildup/mivv/domain/shorts/dto/ShortsDto.java
+++ src/main/java/projectbuildup/mivv/domain/shorts/dto/ShortsDto.java
@@ -1,22 +1,30 @@
 package projectbuildup.mivv.domain.shorts.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import projectbuildup.mivv.domain.shorts.entity.Shorts;
 import projectbuildup.mivv.domain.shorts.entity.ShortsType;
 import projectbuildup.mivv.domain.shorts.service.ShortsService;
 
 public class ShortsDto {
-    @Getter
-    public class idRequest{
+    @Getter @AllArgsConstructor @NoArgsConstructor
+    public static class idRequest{
+        @Setter
         Long id;
     }
     @Getter
     public static class creatRequest{
+        @NotBlank(message = "제목은 필수 입력값입니다.")
         String title;
+        @NotBlank(message = "설명은 필수 입력값입니다.")
         String content;
+        @NotBlank(message = "영상 주소는 필수 입력값입니다.")
         String videoUrl;
+        @NotNull(message = "영상 타입은 필수 입력값입니다.")
         ShortsType type;
     }
     @Getter
