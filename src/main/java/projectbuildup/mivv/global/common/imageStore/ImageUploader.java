4b9c7fef66626a16dbfc2938ef85171aee7b74ca
hyuns2 <109636910+hyuns2@users.noreply.github.com>
2023-05-06 13:17:53 +0900

    Update ImageUploader.java

--- src/main/java/projectbuildup/mivv/global/common/imageStore/ImageUploader.java
+++ src/main/java/projectbuildup/mivv/global/common/imageStore/ImageUploader.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import projectbuildup.mivv.global.error.exception.CUnknownIpException;
 
 import java.io.File;
 import java.io.IOException;
