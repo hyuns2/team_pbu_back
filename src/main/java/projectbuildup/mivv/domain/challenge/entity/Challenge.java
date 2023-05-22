cf6ede020ddb3c88ba5b66206132fe0ca68dee58
이한솔 <hansol8701@naver.com>
2023-05-22 21:19:08 +0900

    refactor : challenge 엔티티 컬럼명/테이블명 설정

--- src/main/java/projectbuildup/mivv/domain/challenge/entity/Challenge.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/entity/Challenge.java
@@ -3,6 +3,7 @@ package projectbuildup.mivv.domain.challenge.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Columns;
 import projectbuildup.mivv.domain.challenge.dto.ChallengeDto;
 import projectbuildup.mivv.domain.participation.entity.Participation;
 import projectbuildup.mivv.global.common.imageStore.Image;
@@ -18,21 +19,38 @@ import java.util.List;
 @Getter
 @Builder
 @ToString
+@Table(name = "challenge")
 public class Challenge extends BaseTimeEntity {
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+    @Column(name = "main_title")
     private String mainTitle;
+    @Column(name = "sub_title")
     private String subTitle;
+    @CollectionTable(
+            name = "challenge_description",
+            joinColumns = @JoinColumn(name = "challenge_id", referencedColumnName = "id")
+    )
+    @Column(name = "description")
     @ElementCollection
     private List<String> description = new ArrayList<>();
+    @Column(name = "max_saving_amount")
     private long maxSavingAmount;
+    @Column(name = "min_saving_amount")
     private long minSavingAmount;
+    @Column(name = "limited_number_of_times")
     private long limitedNumberOfTimes;
+    @Columns(columns = {
+            @Column(name = "original_image_name"),
+            @Column(name = "image_path"),
+            @Column(name = "store_image_name")
+    })
     private Image image;
-
+    @Column(name = "start_date")
     private LocalDate startDate;
+    @Column(name = "end_date")
     private LocalDate endDate;
 
     @OneToMany(mappedBy = "challenge", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
