2ec6e1a11a886afef948fdffd5a5f8f3df0c9d85
이한솔 <hansol8701@naver.com>
2023-05-24 00:05:31 +0900

    refactor : IdentityVerification 엔티티 컬럼명 설정

--- src/main/java/projectbuildup/mivv/domain/user/entity/IdentityVerification.java
+++ src/main/java/projectbuildup/mivv/domain/user/entity/IdentityVerification.java
@@ -13,17 +13,23 @@ import projectbuildup.mivv.global.common.BaseTimeEntity;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "identity_verification")
 public class IdentityVerification extends BaseTimeEntity {
     private final static String DUMMY_NAME = "심재헌";
     private final static String DUMMY_BIRTHDATE = "19990101";
     private final static String DUMMY_MOBILE = "01000000000";
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Column(name = "name")
     String name;
+    @Column(name = "birth_date")
     String birthDate;
+    @Column(name = "mobile")
     String mobile;
+    @Column(name = "code")
     String code;
     @OneToOne(mappedBy = "identityVerification", fetch = FetchType.LAZY)
     User user;
