5437f386ea57fe6951d5a466e95548f20a7f8420
이한솔 <hansol8701@naver.com>
2023-05-31 08:46:48 +0900

    refactor : Participation&Remittance 엔티티 Soft Delete 적용

--- src/main/java/projectbuildup/mivv/domain/participation/entity/Participation.java
+++ src/main/java/projectbuildup/mivv/domain/participation/entity/Participation.java
@@ -4,19 +4,23 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import projectbuildup.mivv.domain.remittance.entity.Remittance;
 import projectbuildup.mivv.domain.challenge.entity.Challenge;
 import projectbuildup.mivv.domain.saving_count.entity.SavingCount;
 import projectbuildup.mivv.domain.user.entity.User;
 import projectbuildup.mivv.global.common.BaseTimeEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Getter
-@Setter
 @NoArgsConstructor
 @Entity
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE participation SET deleted_at = CURRENT_TIMESTAMP where id = ?")
 @Table(name = "participation")
 public class Participation extends BaseTimeEntity {
 
@@ -33,12 +37,15 @@ public class Participation extends BaseTimeEntity {
     @JoinColumn(name = "challenge_id")
     private Challenge challenge;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "saving_count_id")
-    SavingCount savingCount;
+    private SavingCount savingCount;
 
     @OneToMany(mappedBy = "participation", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Remittance> remittanceList;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
     public Participation(User user, Challenge challenge){
         this.user = user;
@@ -49,5 +56,9 @@ public class Participation extends BaseTimeEntity {
     public boolean canRemit() {
         long limit = challenge.getLimitedNumberOfTimes();
         return savingCount.getCount() < limit;
+    }
+
+    public void setSavingCount(SavingCount savingCount) {
+        this.savingCount = savingCount;
     }
 }
