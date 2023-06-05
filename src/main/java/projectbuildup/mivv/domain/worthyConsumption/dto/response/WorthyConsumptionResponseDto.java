17498df63889093fec54f36616238418148603b6
ZZKIM <rkgml424@naver.com>
2023-06-05 17:54:15 +0900

    가치소비 추천이유 LIST로 다시 구현 : id 빼달라고 해서 뺌

--- src/main/java/projectbuildup/mivv/domain/worthyConsumption/dto/response/WorthyConsumptionResponseDto.java
+++ src/main/java/projectbuildup/mivv/domain/worthyConsumption/dto/response/WorthyConsumptionResponseDto.java
@@ -1,12 +1,16 @@
 package projectbuildup.mivv.domain.worthyConsumption.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import projectbuildup.mivv.domain.worthyConsumption.entity.CheckConditionType;
 import projectbuildup.mivv.domain.worthyConsumption.entity.RecommendationReason;
 import projectbuildup.mivv.domain.worthyConsumption.entity.WorthyConsumption;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Schema(description = "가치소비 응답 DTO")
 public class WorthyConsumptionResponseDto {
     @NoArgsConstructor @AllArgsConstructor
@@ -20,6 +24,7 @@ public class WorthyConsumptionResponseDto {
         private Long lastMonthAmount;
         private Integer maxParticipants;
         private List<String> summary;
+        private String logoPath;
 
         public ReadSummaryResponse(WorthyConsumption worthyConsumption) {
             this.id = worthyConsumption.getId();
@@ -30,13 +35,13 @@ public class WorthyConsumptionResponseDto {
             this.lastMonthAmount = worthyConsumption.getCondition().getLastMonthAmount();
             this.maxParticipants = worthyConsumption.getCondition().getMaxParticipants();
             this.summary = worthyConsumption.getSummary();
+            this.logoPath = worthyConsumption.getWorthyConsumptionUrl().getLogoPath();
         }
     }
     @NoArgsConstructor @AllArgsConstructor
     @Getter @Setter
     public static class ReadBasicResponse extends ReadSummaryResponse{
         private List<String> hashtags;
-        private String logoPath;
         private String videoThumbNailPath;
         private String videoPath;
         private String imagePath;
@@ -46,7 +51,6 @@ public class WorthyConsumptionResponseDto {
 
         public ReadBasicResponse(WorthyConsumption worthyConsumption, Boolean isLiked, Long couponId) {
             super(worthyConsumption);
-            this.logoPath = worthyConsumption.getWorthyConsumptionUrl().getLogoPath();
             this.videoThumbNailPath = worthyConsumption.getWorthyConsumptionUrl().getVideoThumbNailPath();
             this.hashtags = worthyConsumption.getHashtags();
             this.videoPath = worthyConsumption.getWorthyConsumptionUrl().getVideoPath();
@@ -68,7 +72,7 @@ public class WorthyConsumptionResponseDto {
         private String detailImageUrl;
         private String detailBackgroundImageUrl;
         private String videoUrl;
-        private List<RecommendationReason> recommendationReasons;
+        private List<RecommendationReasonDto> recommendationReasons;
         private Integer originalPrice;
         private Integer salePrice;
         private String availablePrice;
@@ -84,7 +88,9 @@ public class WorthyConsumptionResponseDto {
             this.detailImageUrl = worthyConsumption.getWorthyConsumptionUrl().getDetailImagePath();
             this.detailBackgroundImageUrl = worthyConsumption.getWorthyConsumptionUrl().getDetailBackgroundImagePath();
             this.videoUrl = worthyConsumption.getWorthyConsumptionUrl().getVideoPath();
-            this.recommendationReasons = worthyConsumption.getRecommendationReasons();
+            this.recommendationReasons = worthyConsumption.getRecommendationReasons().stream()
+                    .map(RecommendationReasonDto::new)
+                    .collect(Collectors.toList());
             this.originalPrice = worthyConsumption.getOriginalPrice();
             this.salePrice = worthyConsumption.getSalePrice();
             this.availablePrice = worthyConsumption.getAvailablePrice();
@@ -93,5 +99,16 @@ public class WorthyConsumptionResponseDto {
         }
 
 
+    }
+    @NoArgsConstructor @AllArgsConstructor
+    @Getter @Setter
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    public static class RecommendationReasonDto{
+        String title;
+        String description;
+        public RecommendationReasonDto(RecommendationReason recommendationReason){
+            this.title = recommendationReason.getTitle();
+            this.description = recommendationReason.getDescription();
+        }
     }
 }
