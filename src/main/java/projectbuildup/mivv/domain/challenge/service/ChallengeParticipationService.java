b3b6d0514c3171558e3e94b1925c5409c903dccb
이한솔 <hansol8701@naver.com>
2023-06-15 23:11:17 +0900

    feat: 참여 종료된 챌린지 목록을 조회하는 기능 추가

--- src/main/java/projectbuildup/mivv/domain/challenge/service/ChallengeParticipationService.java
+++ src/main/java/projectbuildup/mivv/domain/challenge/service/ChallengeParticipationService.java
@@ -27,7 +27,8 @@ public class ChallengeParticipationService {
      * 참여 중인 챌린지 목록을 조회합니다.
      * 종료된 챌린지는 제외합니다.
      *
-     * @param userId 회원 아이디넘버
+     * @param userId    회원 아이디넘버
+     * @param pageParam 페이지 필드
      * @return 참여 중인 챌린지 목록
      */
     public PagingDto<ChallengeDto.Response> getOngoingChallenges(Long userId, PageParam pageParam) {
@@ -41,7 +42,8 @@ public class ChallengeParticipationService {
     /**
      * 참여 가능한 챌린지 목록을 조회합니다.
      *
-     * @param userId 사용자 아이디넘버
+     * @param userId    사용자 아이디넘버
+     * @param pageParam 페이지 필드
      * @return 참여 가능한 챌린지 목록
      */
     public PagingDto<ChallengeDto.Response> getJoinableChallenges(Long userId, PageParam pageParam) {
@@ -51,4 +53,17 @@ public class ChallengeParticipationService {
         return challengeService.challengesToResponseDto(pages.getNumber(), pages.getTotalPages(), pages.getContent());
     }
 
+    /**
+     * 참여했던 챌린지 중, 종료된 것을 조회합니다.
+     *
+     * @param userId    사용자 아이디넘버
+     * @param pageParam 페이지 필드
+     * @return 참여했던 챌린지 목록
+     */
+    public PagingDto<ChallengeDto.Response> getClosedChallenges(Long userId, PageParam pageParam) {
+        Pageable pageable = pageParam.toPageable();
+        User user = userRepository.findById(userId).orElseThrow(CUserNotFoundException::new);
+        Page<Participation> pages = participationRepository.findAllByUserAndClosedTrue(user, pageable);
+        return challengeService.participationsToResponseDto(pages.getNumber(), pages.getTotalPages(), pages.getContent());
+    }
 }
