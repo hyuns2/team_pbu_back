1c25723f32e674ee593469981259ef472a52ffe3
hyuns2 <hyunsoi0103@gmail.com>
2024-03-11 19:37:53 +0900

    Refactor: update InquiryService

--- src/main/java/projectbuildup/mivv/domain/inquiry/service/InquiryService.java
+++ src/main/java/projectbuildup/mivv/domain/inquiry/service/InquiryService.java
@@ -91,6 +91,7 @@ public class InquiryService {
      * @param id 문의 id
      * @throws CInquiryNotFoundException 문의 찾기 실패시
      */
+    @Transactional
     public void deleteInquiry(final User user, final Long id) {
         Optional<InquiryEntity> target = repo.findById(id);
         if (target.isEmpty()) {
